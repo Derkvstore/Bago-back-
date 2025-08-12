804ea118066905d8d1a20dfb72fd1bb29a14bdf1
Abdel Kader Diarra <abdelkaderdiarra707@gmail.com>
2025-08-12 12:02:52 +0000

    Correction de benefices

--- generate-hash.js
+++ generate-hash.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
 
-bcrypt.hash('Yatt6888', 10).then(hash => {
+bcrypt.hash('Bago6888', 10).then(hash => {
   console.log('Hash généré :', hash);
 });
--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/api/reports/dashboard-stats', async (req, res) => {
 });
 
 
-// Route bénéfices
+// Route bénéfices (corrigée)
 app.get('/api/benefices', async (req, res) => {
   try {
     let query = `
@@ -115,11 +115,9 @@ app.get('/api/benefices', async (req, res) => {
           vente_items vi
       JOIN
           ventes v ON vi.vente_id = v.id
-      JOIN
-          factures f ON v.id = f.vente_id
       WHERE
           vi.statut_vente = 'actif'
-          AND f.statut_facture = 'payee_integralement'
+          AND v.statut_paiement = 'payee_integralement'
     `;
 
     const queryParams = [];
